971fc9561de1515f3e64015a59287210379a2fd2
Razvan Vasile <vasile.razvan93@gmail.com>
2019-06-24 03:02:42 +0300

    i promise this is the last one

--- server/src/routes/case.js
+++ server/src/routes/case.js
@@ -1,9 +1,9 @@
 module.exports = app => {
   const cases = require("../controllers/case");
 
-  app.get("api/cases/:caseCode", cases.findOne);
+  app.get("cases/:caseCode", cases.findOne);
 
-  app.post("api/cases", cases.create);
+  app.post("cases", cases.create);
 
-  app.delete("api/cases/:caseCode", cases.delete);
+  app.delete("cases/:caseCode", cases.delete);
 };
