430ba48385ca03e58dfc6187b1ec00f09cdc3416
cojeagabriel <cojeagabriel@yahoo.com>
2019-06-25 20:50:51 +0300

    home screen added

--- client/barem-app/src/app/app.component.ts
+++ client/barem-app/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { untilDestroyed } from 'ngx-take-until-destroy';
+import { Router, NavigationStart } from '@angular/router';
+import { Event as NavigationEvent } from "@angular/router";
+import { filter } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'barem-app';
+
+  constructor(
+    router: Router
+  ){
+    router.events
+      .pipe(
+        filter(
+          (event: NavigationEvent) => {
+            return (event instanceof NavigationStart);
+          }
+        ),
+        untilDestroyed(this)
+      )
+      .subscribe(
+        (event: NavigationStart) => {
+
+          console.group("NavigationStart Event");
+
+          console.log("navigation id:", event.id);
+          console.log("route:", event.url);
+          console.log("trigger:", event.navigationTrigger);
+
+          if (event.restoredState) {
+
+            console.warn(
+              "restoring navigation id:",
+              event.restoredState.navigationId
+            );
+
+          }
+          console.groupEnd();
+        }
+      );
+  }
+
+  ngOnDestroy(){
+    
+  }
 }
