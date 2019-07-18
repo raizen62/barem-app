769697492e7db4fc213ccf5a738a19755ec8af79
Razvan Vasile <vasile.razvan93@gmail.com>
2019-07-19 02:04:34 +0300

    changed the cors settings from the server

--- server/src/app.js
+++ server/src/app.js
@@ -17,9 +17,17 @@ app.use(bodyParser.json());
 
 // Dam enable la CORS
 app.use(cors());
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    next();
 });
 
 //Routes
