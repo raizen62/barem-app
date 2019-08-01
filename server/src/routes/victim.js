2caad059fb199bdf62bfd2b923094eb565935c42
Razvan Vasile <razvan.vasile@finastra.com>
2019-08-01 12:10:38 +0300

    fixed endpoint

--- server/src/routes/victim.js
+++ server/src/routes/victim.js
@@ -1,7 +1,7 @@
 module.exports = app => {
   const victims = require("../controllers/victim");
 
-  app.get("/victims/:context", victims.findAllByContext);
+    app.get("/victims/context/:context", victims.findAllByContext);
 
     app.get("/victims/:id", victims.findOne);
   
