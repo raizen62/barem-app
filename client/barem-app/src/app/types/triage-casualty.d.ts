60864d7d16125b598f6b91fc8f720e158b95c269
cojeagabriel <cojeagabriel@yahoo.com>
2019-08-15 00:40:51 +0300

    triage first version

---
+++ client/barem-app/src/app/types/triage-casualty.d.ts
@@ -0,0 +1,11 @@
+export interface TriageCasualty {
+    name?: string,
+    age?: number,
+    canWalk?: boolean,
+    breathing?: number,
+    openAirway?: boolean,
+    insufflations?: boolean,
+    TRC?: number,
+    mentalStatus?: boolean,
+    tag?: string
+}
